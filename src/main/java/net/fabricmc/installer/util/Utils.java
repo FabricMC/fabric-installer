02c87570135e631fa21bfe29ce95749d15717afb
modmuss50 <modmuss50@gmail.com>
2022-09-01 22:49:48 +0100

    Support Linux Flatpack version of the Minecraft Launcher.
    See: https://github.com/flathub/com.mojang.Minecraft

--- src/main/java/net/fabricmc/installer/util/Utils.java
+++ src/main/java/net/fabricmc/installer/util/Utils.java
@@ -71,6 +71,15 @@ public class Utils {
 				dir = homeDir.resolve("Library").resolve("Application Support").resolve("minecraft");
 			} else {
 				dir = homeDir.resolve(".minecraft");
+
+				if (OperatingSystem.CURRENT == OperatingSystem.LINUX && !Files.exists(dir)) {
+					// https://github.com/flathub/com.mojang.Minecraft
+					final Path flatpack = homeDir.resolve(".var").resolve("app").resolve("com.mojang.Minecraft").resolve(".minecraft");
+
+					if (Files.exists(flatpack)) {
+						dir = flatpack;
+					}
+				}
 			}
 		}
 
